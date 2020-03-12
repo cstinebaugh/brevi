4da8bf5b956078d25063f92ce715185a2f0769b0
Caroline Stinebaugh <carolinestinebaugh@Carolines-MacBook-Pro.local>
2020-03-12 15:59:59 -0400

    abc and fox

--- crawler/abc.py
+++ crawler/abc.py
@@ -4,29 +4,25 @@ import requests
 
 # return a dictionary with site name and url
 def get_site_info():
-	return {'site': 'Fox News', 'url': 'https://www.foxnews.com/politics/'}
-
+	return {'site': 'ABC', 'url': 'https://abcnews.go.com/politics/'}
 
 # return a list of headline objects. You can choose how to represent headlines
 # but make sure your object can have url and title extracted from it by your 
 # other helper functions
 def get_headlines(soup):
-	return soup.find_all('h4', class_='title')
+	return soup.find_all(True, class_='headlines-li')
 
 	
 # return the url for the article corresponding to this headline
 def get_headline_url(headline):
+	current_headline = headline.find('h1')
 	url = headline.find('a').get('href')
-	if url[0:4] != 'http':
-		url = urljoin('https://foxnews.com', url)
 	
 	return url
-
 
 # return the text of the headline object
 def get_headline_text(headline):
 	return headline.find('a').get_text()
-
 
 # return a dictionary for the article containing:
 # {
@@ -43,14 +39,15 @@ def process_headline(headline):
 	result['title'] = get_headline_text(headline)
 	article = requests.get(result['url']).content
 	soup_article = BeautifulSoup(article, 'lxml')
-	body = soup_article.find_all('div', class_='article-body')[0]
+
+	body = soup_article.find_all('div', class_='Article')[0]
 	paragraph_objs = body.find_all('p')
 	paragraph_texts = [p.get_text() for p in paragraph_objs]
-	date = soup_article.find('meta', attrs={'name':"dcterms.created"}).get("content")
-	result['time'] = date
+	date = soup_article.find('meta', property="lastPublishedDate").get("content")
+
 	result['text'] = '\n\n'.join(paragraph_texts)
-	print(result)
-	
+	result['time'] = date
+
 	return result
 
 
