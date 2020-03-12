1e557df0d2160ee02270f47f618daa0ccf04a6e5
Jonson Jin <>
2020-03-12 15:40:48 -0400

    Crawler working for atlantic and washington times

--- crawler/washington_times.py
+++ crawler/washington_times.py
@@ -1,25 +1,31 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import datetime
+import json
 import requests
 
 # return a dictionary with site name and url
 def get_site_info():
-	return # TODO
+	return {'site': 'The Washington Times', 'url': 'https://www.washingtontimes.com/news/politics/'}
 
 # return a list of headline objects. You can choose how to represent headlines
 # but make sure your object can have url and title extracted from it by your 
 # other helper functions
 def get_headlines(soup):
-	return # TODO
+	return soup.find_all('h2', class_='article-headline')
 
 	
 # return the url for the article corresponding to this headline
 def get_headline_url(headline):
-	return # TODO
+	url = headline.find('a').get('href')
+	if url[0:4] != 'http':
+		url = urljoin('https://www.washingtontimes.com', url)
+	return url
 
 
 # return the text of the headline object
 def get_headline_text(headline):
-	return # TODO
+	return headline.find('a').get_text()
 
 
 # return a dictionary for the article containing:
@@ -31,7 +37,25 @@ def get_headline_text(headline):
 #   text:  article content
 # }
 def process_headline(headline):
-	return # TODO
+	result = {}
+	result['site'] = get_site_info()['site']
+	result['url'] = get_headline_url(headline)
+	result['title'] = get_headline_text(headline)
+	
+	# download article and parse html to get its text
+	article = requests.get(result['url']).content
+	soup_article = BeautifulSoup(article, 'lxml')
+
+	data = json.loads(soup_article.find('script', type='application/ld+json').text)
+	result['time'] = data['datePublished']
+	
+	soup_article.find('div',class_='permission').decompose()
+	body = soup_article.find_all('div', class_='bigtext')[0]
+	paragraph_objs = body.find_all('p')
+	paragraph_texts = [p.get_text() for p in paragraph_objs]
+	
+	result['text'] = '\n\n'.join(paragraph_texts)
+	return result
 
 
 # dictionary object for calling functions
